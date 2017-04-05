c74bb9299cb9bd0f01474136a6136562a1285bdc
Isaac Selleck (RIT Student) <ixs5214@ad.rit.edu>
2017-04-05 18:14:20 -0400

    fixed typo

--- server/router.js
+++ server/router.js
@@ -2,14 +2,14 @@ const controllers = require('./controllers');
 const mid = require('./middleware');
 
 const router = (app) => {
-    app.get('/login', mid.requireSecure, mid.requireLogout, controllers.Account.loginPage);
-    app.post('/login', mid.requireSecure, mid.requireLogout, controllers.Account.login);
-    app.get('/signup', mid.requireSecure, mid.requireLogout, controllers.Account.signupPage);
-    app.post('/signup', mid.requireSecure, mid.requireLogout, controllers.Account.signup);
-    app.get('/logout', mid.requireLogin, controllers.Account.logout);
-    app.get('/maker', mid.requireLogin, controllers.Domo.makerPage);
-    app.post('/maker', mid.requireLogin, controllers.Domo.make);
-    app.get('/', mid.requireSecure, mid.requireLogout, controllers.Account.loginPage);
+    app.get('/login', mid.requireSecure, mid.requiresLogout, controllers.Account.loginPage);
+    app.post('/login', mid.requireSecure, mid.requiresLogout, controllers.Account.login);
+    app.get('/signup', mid.requireSecure, mid.requiresLogout, controllers.Account.signupPage);
+    app.post('/signup', mid.requireSecure, mid.requiresLogout, controllers.Account.signup);
+    app.get('/logout', mid.requiresLogin, controllers.Account.logout);
+    app.get('/maker', mid.requiresLogin, controllers.Domo.makerPage);
+    app.post('/maker', mid.requiresLogin, controllers.Domo.make);
+    app.get('/', mid.requireSecure, mid.requiresLogout, controllers.Account.loginPage);
 };
 
 module.exports = router;
